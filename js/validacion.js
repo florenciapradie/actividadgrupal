632a05b27445776bb30dc97a2e59552cc7e3c373
Leandro <leandroperez27306@gmail.com>
2025-08-14 14:20:06 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -1,13 +1,13 @@
 function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
+    document.getElementsById("alert-success").classList.add("show");
 }
 
 function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
+    document.getElementsById("alert-danger").classList.add("show");
 
 }
 function notnull(nombre,apellido,email,password1,password2){
-if (nombre === "" || apellido === "" || email === "" || pass1 === "" || pass2 === "") {
+if (nombre === "" || apellido === "" || email === "" || password1 === "" || password2 === "") {
   showAlertError();
   return;
     }
@@ -37,12 +37,12 @@ if(!checkbox.checked){
 }
 
 function validarFormulario() {
-    let nombre = document.getElementById("nombre").value.trim();
-    let apellido = document.getElementById("apellido").value.trim();
-    let email = document.getElementById("email").value.trim();
-    let pass1 = document.getElementById("password1").value;
-    let pass2 = document.getElementById("password2").value;
-    let checkbox = document.getElementById("terminos");
+    let nombre = document.getElementsById("nombre").value.trim();
+    let apellido = document.getElementsById("apellido").value.trim();
+    let email = document.getElementsById("email").value.trim();
+    let pass1 = document.getElemenstById("password1").value;
+    let pass2 = document.getElementsById("password2").value;
+    let checkbox = document.getElementsById("terminos");
 
     if (
         notnull(nombre, apellido, email, pass1, pass2) &&
@@ -53,3 +53,4 @@ function validarFormulario() {
         showAlertSuccess();
     }
 }
+
